0ec7e88dc79bc8227dd181bddb8e2fbf25be7bc2
Benn9211 <bhanupartaps38@gmail.com>
2017-09-16 00:01:10 -0700

    Update newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -2,22 +2,22 @@
 import psycopg2
 
 
-# What are the most popular three articles of all time?
-query_1_title = ("What are the most popular three articles of all time?")
+# What are the most popular three Mangas of all time?
+query_1_title = ("What are the most popular three mangas of all time?")
 query_1 = (
-    "select articles.title, count(*) as views "
-    "from articles inner join log on log.path "
+    "select mangas.title, count(*) as views "
+    "from mangas inner join log on log.path "
     "like concat('%', articles.slug, '%') "
     "where log.status like '%200%' group by "
-    "articles.title, log.path order by views desc limit 3")
+    "mangas.title, log.path order by views desc limit 3")
 
-# Who are the most popular article authors of all time?
-query_2_title = ("Who are the most popular article authors of all time?")
+# Who are the most popular mangas authors of all time?
+query_2_title = ("Who are the most popular mangas authors of all time?")
 query_2 = (
-    "select authors.name, count(*) as views from articles inner "
-    "join authors on articles.author = authors.id inner join log "
+    "select authors.name, count(*) as views from mangas inner "
+    "join authors on mangas.author = authors.id inner join log "
     "on log.path like concat('%', articles.slug, '%') where "
-    "log.status like '%200%' group "
+    "log.status like '%100%' group "
     "by authors.name order by views desc")
 
 # On which days did more than 1% of requests lead to errors
@@ -27,7 +27,7 @@ query_3 = (
     "select day, round((sum(requests)/(select count(*) from log where "
     "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as "
     "perc from (select substring(cast(log.time as text), 0, 11) as day, "
-    "count(*) as requests from log where status like '%404%' group by day)"
+    "count(*) as requests from log where status like '%50%' group by day)"
     "as log_percentage group by day order by perc desc) as final_query "
     "where perc >= 1")
 
@@ -66,7 +66,7 @@ def print_error_results(query_results):
 
 if __name__ == '__main__':
     # store query results
-    popular_articles_results = get_query_results(query_1), query_1_title
+    popular_mangas_results = get_query_results(query_1), query_1_title
     popular_authors_results = get_query_results(query_2), query_2_title
     load_error_days = get_query_results(query_3), query_3_title
 
